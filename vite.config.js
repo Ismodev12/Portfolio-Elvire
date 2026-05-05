3dadc34fad3822e167898c9f77d97464fa44a160
ismodev12 <salifoukayodeism@gmail.com>
2026-05-05 04:47:06 +0100

    Fix base URL for Netlify

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-elvire/', // ← remplace par le nom exact de ton dépôt GitHub
+  base: '/',
 })
